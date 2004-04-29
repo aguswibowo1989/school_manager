5244c2ec4fef3b0e4361a525fd5b1b0bd577c5fb
Greg Hewett <greg@strand3.com>
2004-04-29 01:19:15 +0000

    Open does not delet.
    git-svn-id: http://strand3.com/svn/private/trunk/SchoolManager@224 42119495-8021-0410-92ea-b7df3c3667d7

--- Site/Applications/Curriculum/OpenResource.php
+++ Site/Applications/Curriculum/OpenResource.php
@@ -18,44 +18,6 @@
         trigger_error("Resource ID is a require parameter", E_USER_ERROR);
     }
     
-    if ($action == "Yes") {
-        $sql = "delete from resource where id = '$resourceid'";
-        $result = $conn->query($sql);
-        
-        if (DB::isError($result)) {
-            trigger_error($sql);
-            trigger_error($result->getMessage());
-            trigger_error("Cound not delete from resource table" , E_USER_ERROR);
-        }
-        $sql = "delete from lstr where resourceid = '$resourceid'";
-        $result = $conn->query($sql);
-        
-        if (DB::isError($result)) {
-            trigger_error($sql);
-            trigger_error($result->getMessage());
-            trigger_error("Could not delete from lstr table" , E_USER_ERROR);
-        }
-        
-        header("Location:  ViewResources.php?" . http_build_simple_query($vars));
-    }
-    elseif ($action == "No") {
-        header("Location:  ViewResources.php?" . http_build_simple_query($vars));
-    }
-    else {
-        $query = "select name from resource where id = " . $conn->quote($resourceid);
-        $result = $conn->query($query);
-        
-        if (DB::isError($result)) {
-            trigger_error($sql);
-            trigger_error($result->getMessage());
-            trigger_error("Could not name from resource." , E_USER_ERROR);
-        }
-        
-        $row = $result->fetchRow();
-        $name = $row[0];
-        $result->free();
-        
-        layout_display_dialog("Are you sure you want to delete the resource named '{$name}'?", "YesNo", $vars);
-    }
- 
+    header("Location:  ViewResources.php?" . http_build_simple_query($vars));
+   
 ?>
