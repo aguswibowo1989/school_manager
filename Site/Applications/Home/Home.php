da85ff73d22542dd2e471872940094262ec47525
Greg Hewett <greg@strand3.com>
2004-06-09 00:05:13 +0000

    one more quick change.
    git-svn-id: http://strand3.com/svn/private/trunk/SchoolManager@273 42119495-8021-0410-92ea-b7df3c3667d7

--- Site/Applications/Home/Home.php
+++ Site/Applications/Home/Home.php
@@ -4,7 +4,11 @@
     layout_begin();
     
 ?>
-<h2> Welcome Home </h2>
+<h2> Welcome to SchoolManager </h2>
+
+<p>This site aims to help teachers to organize and share lesson plans.  Use the
+<a href="<?= $config['local']['home'] ?>Applications/Curriculum/">Curriculum</a>
+Section of this application to start create lesson plans in this tool.</p>
 
 <?php 
     
