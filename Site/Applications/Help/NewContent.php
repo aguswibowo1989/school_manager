953aa332cc3f044a1b8603342cb1f59582baa559
Greg Hewett <greg@strand3.com>
2004-08-07 21:48:37 +0000

    ho hum
    git-svn-id: http://strand3.com/svn/private/trunk/SchoolManager@285 42119495-8021-0410-92ea-b7df3c3667d7

--- Site/Applications/Help/NewContent.php
+++ Site/Applications/Help/NewContent.php
@@ -34,7 +34,7 @@
 <tr class="FormTable">
 <td class="FormTable">&nbsp;</td>
 <td class="FormTable">
-<input type=submit name=action value="Create Section">
+<input type=submit name=action value="Create Content">
 <input type=submit name=action value="Cancel">
 </td>
 </tr>
